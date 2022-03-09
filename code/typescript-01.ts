cd9942aa41d1995589c24abfcdc5210aca41e468
Joao Oliveira <thxjao.dev@gmail.com>
2022-03-09 19:47:12 -0300

    feat: added two classes notes

---
+++ code/typescript-01.ts
@@ -0,0 +1,9 @@
+/*
+Some benefits of using Typescript: 
+    - It's a open source technologie.
+    - Follows the ECMAScript standard.  
+    - Have a innovating and Static typing (What is static Typing?). --
+    - Continuos maintenance. 
+    - Developed and maintaned by the technical Team.
+    - It's a languange that is growing day after day.
+*/
---
+++ code/typescript-02.ts
@@ -0,0 +1,7 @@
+/*
+Learning a little about the typescript playground (online). 
+
+
+    Important point:
+        Typescript uses "use strict" by default at the start of the JS file, what's the motive behined this? --
+*/
